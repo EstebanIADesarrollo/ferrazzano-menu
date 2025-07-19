c8d35aee32a0728c550af2b45e1181c0545e560c
EstebanIADesarrollo <esteban.avh.test@gmail.com>
2025-07-19 16:36:29 -0500

    feat: Implementar seccion de beneficios y unificar estilos de tarjetas
    - Se crea el componente  y se establece como la ruta principal de la aplicacion.
    - Se actualiza el enrutamiento para que el logotipo de la aplicacion redirija a la seccion de beneficios.
    - Se unifican los estilos de las tarjetas de los componentes  y  para mantener una apariencia visual coherente en toda la aplicacion.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,10 @@
 import { Routes } from '@angular/router';
 import { Panes } from './components/panes/panes';
 import { Pizzas } from './components/pizzas/pizzas';
+import { Beneficios } from './components/beneficios/beneficios';
 
 export const routes: Routes = [
-    { path: '', redirectTo: 'panes', pathMatch: 'full' },
+    { path: '', component: Beneficios },
     { path: 'panes', component: Panes },
     { path: 'pizzas', component: Pizzas },
 ];
