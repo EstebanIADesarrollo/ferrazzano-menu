b7882687720669a7fe4a2d09f552609c0a34e219
EstebanIADesarrollo <esteban.avh.test@gmail.com>
2025-07-21 22:01:37 -0500

    refactor: Actualiza extensiones de imagen a .webp en el servicio de productos

--- src/app/services/productos.ts
+++ src/app/services/productos.ts
@@ -11,49 +11,49 @@ export class Productos {
       "descripcion": "Pan rústico con corteza crocante y miga suave, ideal para acompañar comidas.",
       "peso": 500,
       "precio": 10000,
-      "nombre_de_imagen": "pan_de_campo.jpeg"
+      "nombre_de_imagen": "pan_de_campo.webp"
     },
     {
       "nombre": "Pan de Chocolate",
       "descripcion": "Pan con chips de chocolate semiamargo, perfecto para desayunos o meriendas.",
       "peso": 500,
       "precio": 13000,
-      "nombre_de_imagen": "pan_de_chocolate.jpeg"
+      "nombre_de_imagen": "pan_de_chocolate.webp"
     },
     {
       "nombre": "Pan de Arándanos",
       "descripcion": "Pan artesanal con arándanos deshidratados, suave y ligeramente dulce.",
       "peso": 500,
       "precio": 14000,
-      "nombre_de_imagen": "pan_de_arandanos.png"
+      "nombre_de_imagen": "pan_de_arandanos.webp"
     },
     {
       "nombre": "Pan Multicereal",
       "descripcion": "Pan saludable elaborado con mezcla de cereales y semillas, fuente de fibra.",
       "peso": 500,
       "precio": 15000,
-      "nombre_de_imagen": "pan_multicereal.png"
+      "nombre_de_imagen": "pan_multicereal.webp"
     },
     {
       "nombre": "Pan Bagette Pequeño",
       "descripcion": "Bagette personal con corteza crujiente y miga aireada.",
       "peso": 180,
       "precio": 3000,
-      "nombre_de_imagen": "pan_bagette.jpeg"
+      "nombre_de_imagen": "pan_bagette.webp"
     },
     {
       "nombre": "Pan Bagette Grande",
       "descripcion": "Bagette tradicional de gran tamaño, ideal para compartir.",
       "peso": 300,
       "precio": 5500,
-      "nombre_de_imagen": "pan_bagette.jpeg"
+      "nombre_de_imagen": "pan_bagette.webp"
     },
     {
       "nombre": "Hogaza Queen",
       "descripcion": "Pan rústico con corteza crocante y miga suave, de mas de una libra ideal para compartir.",
       "peso": 800,
       "precio": 15000,
-      "nombre_de_imagen": "hogaza_queen.jpeg"
+      "nombre_de_imagen": "hogaza_queen.webp"
     }
   ];
   
